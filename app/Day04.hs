3f6dcee535ffa739cb9e1ca417062f431b80521a
Robin Daumann <26201853+robindaumann@users.noreply.github.com>
2021-11-29 10:31:44 +0100

    simplify day4 part2

--- app/Day04.hs
+++ app/Day04.hs
@@ -1,14 +1,15 @@
-import Data.List(nub, sort)
+import Data.List (nub, sort)
 
-main :: IO()
+main :: IO ()
 main = do
-    pws <- getContents
-    print . countValid (not . hasDuplicates) $ pws
-    print . countValid (not . hasAnagrams) $ pws
-    where countValid f pws = length . filter f . map words . lines $ pws
+  pws <- getContents
+  print $ countValid (not . hasDuplicates) pws
+  print $ countValid (not . hasAnagrams) pws
+  where
+    countValid f pws = length . filter f . map words . lines $ pws
 
 hasDuplicates :: Eq a => [a] -> Bool
 hasDuplicates l = nub l /= l
 
 hasAnagrams :: Ord a => [[a]] -> Bool
-hasAnagrams l = let filtered = nub $ map sort l in length filtered /= length l
+hasAnagrams l = hasDuplicates $ map sort l
